dc063c52bdd4f8a297c456d67c794c24518968e3
clarajsanchez <clarajsanchez@hotmail.com>
2022-11-20 22:14:32 +0100

    ironhack cart and greatest movies

--- labs/js/lab-dom-ironhack-cart-master/lab-dom-ironhack-cart-master/js/index.js
+++ labs/js/lab-dom-ironhack-cart-master/lab-dom-ironhack-cart-master/js/index.js
@@ -1,17 +1,32 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector(".price span").innerHTML;  
+  const quantity = product.querySelector(".quantity input").valueAsNumber;
 
-  //... your code goes here
+  let subtotal = price * quantity;
+  document.querySelector(".subtotal span").innerHTML = subtotal;
+
+  console.log(price, quantity);
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
+
+  const products = document.getElementsByClassName('product');
+  let totalValue = 0;
+
+  for (let product of products) {
+    totalValue += updateSubtotal(product);
+  }
+
+  document.querySelector("#total-value span").innerText = totalValue;
+
+  console.log(products);
 
   // ITERATION 2
   //... your code goes here
@@ -26,17 +41,58 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  const row = target.parentNode.parentNode;
+  const parent = row.parentNode;
+  parent.removeChild(row);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const createRow = document.querySelector(".create-product");
+  let newProdNameInput = createRow.querySelector("input");
+  let newProdNameValue = newProdNameInput.value;
+  let newProdPriceInput = createRow.querySelector("input[type='number]");
+  let newProdPriceValue = Number(newProdPriceInput.valueAsNumber).toFixed(2);
+
+  const newTableRow = document.createElement('tr');
+  newTableRow.className = 'product';
+  newTableRow.innerHTML = `
+  <td class="name">
+  <span>${newProdNameValue}</span>
+  </td>
+  <td class="price">$<span>${newProdPriceValue}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>
+  `;
+
+  const parent = document.querySelector("cart tbody");
+  parent.appendChild(newTableRow);
+
+  const removeBtn = newTableRow.querySelector('btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeBtns = document.querySelectorAll('btn-remove');
+  for (let removeBtn of removeBtns) {
+    removeBtn.addEventListener('click', removeProduct);
+  }
+
+  const createBtn = document.querySelector('#create');
+  if (createBtn) {
+    createBtn.addEventListener('click', createProduct);
+  }
   //... your code goes here
 });
--- labs/js/lab-javascript-greatest-movies/src/movies.js
+++ labs/js/lab-javascript-greatest-movies/src/movies.js
@@ -1,29 +1,64 @@
+// import { movies } from './data';
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
-    const directors = [];
-    for (let i = 0; i < movies.length; i++){   
-        directors[i] = movies[i].director;
+    // const directors = [];
+    // for (let i = 0; i < movies.length; i++){   
+    //     directors[i] = movies[i].director;
         
-    }
-    console.log(directors.length);
-    console.log(movies.length);
-    return directors;
+    // }
+    // console.log(directors.length);
+    // console.log(movies.length);
+    // return directors;
     // let directors = //map!!!
+    const rawList = moviesArray.map((movie) => movie.director);
+    return rawList;
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const dramaMovies = moviesArray.filter(
+        (movie) =>
+            movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
+    let count = 0;
+    for (let i = 0; i < dramaMovies.length; i++){
+        count++;
+    }
+    return count;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0) {
+        return 0;
+    }
+    const arrayWithScore = moviesArray.filter(movie => typeof(movie.score) === "number");
+    const totalScore = arrayWithScore.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue.score;
+    },0);
+    return +(totalScore / moviesArray.length).toFixed(2);
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const dramaMovies = moviesArray.filter(movie => movie.genre.includes ("Drama"));
+    return scoresAverage(dramaMovies);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const copyOfMovies = moviesArray.map((movie) => movie);
+
+    copyOfMovies.sort((a, b) => {
+        if (a.year > b.year) return a.year - b.year;
+        if (a.year < b.year) return a.year - b.year;
+        else return a.title.localCompare(b.title);
+    });
+    return copyOfMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
